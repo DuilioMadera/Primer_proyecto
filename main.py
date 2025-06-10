152d721b28e18a6701fefe6700fc06c52843928e
DuilioMadera <dumadera@gmail.com>
2025-06-10 20:20:16 -0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,13 @@
+# main.py
+
+# Solicita el primer número
+num1 = float(input("Ingrese el primer número: "))
+
+# Solicita el segundo número
+num2 = float(input("Ingrese el segundo número: "))
+
+# Suma los números
+resultado = num1 + num2
+
+# Muestra el resultado
+print("La suma es:", resultado)
